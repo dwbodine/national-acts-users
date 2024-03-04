33e42d26316051d65e7652735a5f8d2f98bd0cbc
dwbodine <dwbodine@gmail.com>
2024-03-04 15:16:24 -0500

    fixed jwt middleware

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,10 @@ import { authRoutes, protectedRoutes } from "./src/router/routes";
 import { clearUser } from "@/lib/userSlice";
 
 export function middleware(request: NextRequest) {
-  const currentUser = request.cookies.get("currentUser")?.value;
-
+  const currentUserCookie = request.cookies.get("currentUser");
   if (
     protectedRoutes.includes(request.nextUrl.pathname) &&
-    (!currentUser || Date.now() > JSON.parse(currentUser).expiredAt)
+    (!currentUserCookie || !currentUserCookie.value)
   ) {
     request.cookies.delete("currentUser");
     const response = NextResponse.redirect(new URL("/login", request.url));
@@ -17,7 +16,7 @@ export function middleware(request: NextRequest) {
     return response;
   }
 
-  if (authRoutes.includes(request.nextUrl.pathname) && currentUser) {
+  if (authRoutes.includes(request.nextUrl.pathname) && currentUserCookie && currentUserCookie.value) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 }
