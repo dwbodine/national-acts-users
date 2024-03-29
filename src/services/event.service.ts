3f039c6b32887fce24af8e5e9d535145004da603
dwbodine <dwbodine@gmail.com>
2024-03-29 14:50:19 -0400

    filtering external events out of users area

--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -18,7 +18,7 @@ export class EventService {
   }
 
   getEvents = async (reportSelection: UserReportSelection): Promise<GetEventsResponse> => {
-    let url = `/user/eventsAndOrdersSecured?sellerId=${reportSelection.seller.sellerId}`;
+    let url = `/user/eventsAndOrdersSecured?excludeExternal=1&sellerId=${reportSelection.seller.sellerId}`;
 
     if (reportSelection.start) {
       url += `&start=${reportSelection.start}`;
