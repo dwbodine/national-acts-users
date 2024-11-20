8f428214269e5b32c1f1800f1c90527def38440a
dwbodine <dwbodine@gmail.com>
2024-11-20 15:23:01 -0500

    fixed line endings

--- components/admin/log/adminLogIndexComponent.tsx
+++ components/admin/log/adminLogIndexComponent.tsx
@@ -4,7 +4,7 @@ import { Col, Container, Row } from 'react-bootstrap';
 
 export default function AdminLogIndex() {
 
-  const [logs, setLogs] = useState<string | undefined>(undefined);
+  const [logs, setLogs] = useState<string[] | undefined>(undefined);
   const { getAllLogs } = useGetLogs();
 
   useEffect(() => {
@@ -12,7 +12,7 @@ export default function AdminLogIndex() {
       if (logs == undefined) {
         getAllLogs().then((response) => {
           if (response.logs && !response.errorMessage) {
-            setLogs(response.logs);
+            setLogs(response.logs.split('\n'));
           }
         });
       }
@@ -22,13 +22,22 @@ export default function AdminLogIndex() {
     };
   });
 
+  let logRows: any[] = [];
+  if (logs && logs.length > 0) {
+    for (const log of logs) {
+      logRows.push(<p>{log}</p>);
+    }
+  }
 
   return (
     <>
       <Container fluid>
         <Row>
+          <h5>API Error Log</h5>
+        </Row>
+        <Row>
           <Col className="log-table">
-          {logs}
+          {logRows}
           </Col>
         </Row>
       </Container>
