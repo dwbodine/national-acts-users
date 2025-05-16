c23e0e3ac101110fe464e1f70a6416af2200ce6f
dwbodine <dwbodine@gmail.com>
2025-05-16 10:57:17 -0400

    uncommented unfinished admin pages

--- components/admin/adminListComponent.tsx
+++ components/admin/adminListComponent.tsx
@@ -75,7 +75,6 @@ export default function AdminList() {
                   Manage Site Settings (global values)
                 </a>
               </li>
-{/*
               <li>
                 <a className="admin-link" onClick={() => goToAdminPage('manage-global-seller-settings')}>
                   Manage Sellers
@@ -86,7 +85,6 @@ export default function AdminList() {
                   Manage Pages
                 </a>
               </li>
-*/}
             </ul>
           </Col>
         </Row>
