6de9a5d002deb5e92f2c4780ef268e0d44d3816c
dwbodine <dwbodine@gmail.com>
2025-11-23 21:02:54 -0500

    calculating total revenue + refunds correctly

--- components/sales/orders/eventDetailComponent.tsx
+++ components/sales/orders/eventDetailComponent.tsx
@@ -574,7 +574,7 @@ export default function EventDetail(props: EditProps) {
                         <td className="vipLabel">Total Revenue:</td>
                         <td>
                           $
-                          {((currentReportSelection.currentDetailEvent.totalRevenue ?? 0)).toFixed(
+                          {((currentReportSelection.currentDetailEvent.totalRevenue ?? 0) - (currentReportSelection.currentDetailEvent.revenueRefunded ?? 0)).toFixed(
                             2,
                           )}
                         </td>
@@ -583,7 +583,7 @@ export default function EventDetail(props: EditProps) {
                         <td className="vipLabel">Total Service Fees:</td>
                         <td>
                           $
-                          {((currentReportSelection.currentDetailEvent.totalServiceFees ?? 0)).toFixed(
+                          {((currentReportSelection.currentDetailEvent.totalServiceFees ?? 0) - (currentReportSelection.currentDetailEvent.serviceFeeRevenueRefunded ?? 0)).toFixed(
                             2,
                           )}
                         </td>
