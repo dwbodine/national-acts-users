a8f5894c253db69510cb5baed073199ff9fe3005
dB <dwbodine@users.noreply.github.com>
2026-05-12 09:07:46 -0400

    Fix lint errors in selection slice tests

--- src/lib/selectionSlices.test.ts
+++ src/lib/selectionSlices.test.ts
@@ -1,130 +1,26 @@
-/* eslint-disable simple-import-sort/imports */
-
 import { describe, expect, it } from 'vitest';
 
-import adminDataReducer, {
-  resetAdminData,
-  setAdminEvents as setAdminDataEvents,
-  setAdminOrders,
-  setAllFaqCategories,
-  setAllFaqs,
-  setAllPages,
-  setPageOrders,
-  setTicketSocketEventsOnly,
-  setTours as setAdminDataTours,
-  setVenues,
-} from './adminDataSelectionSlice';
-import adminEventsReducer, {
-  resetAdminSelection,
-  resetAllAdminEvents,
-  setActiveEventTab,
-  setAdminDateRange,
-  setAdminEvents,
-  setAdminNotes,
-  setExpandedEvent,
-  setExpandedRow,
-  setFocusControl as setAdminEventsFocusControl,
-  setReloadAdminEvents,
-  setShowDeleted as setAdminEventsShowDeleted,
-  setShowHidden as setAdminEventsShowHidden,
-  setShowInactive as setAdminEventsShowInactive,
-  setUpdateListStatus,
-} from './adminEventsSelectionSlice';
-import adminReportsReducer, {
-  resetAdminReports,
-  setReloadReportData,
-  setReportDates,
-} from './adminReportsSelectionSlice';
-import adminReducer, {
-  adminSelectionSlice,
-  resetAdmin,
-  setAdminDates,
-  setAdminEvent,
-  setAdminOrder,
-  setAdminSeller,
-  setAdminSellerId,
-  setAdminTour,
-  setAdminVenue,
-  setAllSellers,
-  setAllSettings,
-  setCountries,
-  setMustSaveEvent,
-  setMustSaveOrder,
-  setMustSavePage,
-  setPageSellerTypes,
-  setPageTypes,
-  setReloadCountries,
-  setReloadEvents as setReloadAdminEventsFlag,
-  setReloadFaqs,
-  setReloadPages,
-  setReloadRoles,
-  setReloadSellerPages,
-  setReloadSellers,
-  setReloadSettings,
-  setReloadTours,
-  setReloadUsers,
-  setReloadVenues,
-  setRoles,
-  setSelectedFaq,
-  setSelectedFaqCategory,
-  setSelectedPage,
-  setSelectedPageType,
-  setSelectedRole,
-  setSelectedUser,
-  setTicketSocketAccounts,
-  setUsers,
-  setVenueSearchTerm,
-} from './adminSelectionSlice';
-import dashboardReducer, {
-  resetDashboard,
-  setCurrentDashboardData,
-  setDashboardDateRange,
-  setReloadDashboardOrders,
-} from './dashboardSelectionSlice';
-import globalReducer, {
-  resetGlobalSettings,
-  setIsLoading,
-  setSaveInProgress,
-} from './globalSelectionSlice';
-import reportReducer, {
-  resetAll,
-  resetSelection,
-  setDateRange,
-  setEventSeller,
-  setEvents,
-  setFocusControl,
-  setForAdmin,
-  setHideOrderRevenue,
-  setHideOrderServiceFees,
-  setHideRevenue,
-  setHideServiceFees,
-  setReloadEvents,
-  setSelectedTourId,
-  setSeller,
-  setShowDeleted,
-  setShowDeletedOrders,
-  setShowHidden,
-  setShowInactive,
-  setShowInactiveOrders,
-  setShowOnlyEmails,
-  setShowOnlyPhones,
-  setTours,
-} from './reportSelectionSlice';
-import userActivityReducer, {
-  resetUserActivity,
-  setCurrentActivities,
-  setCurrentLogins,
-  setFilterAdmins,
-  setReloadActivities,
-  setUserActivityDateRange,
-} from './userActivitySelectionSlice';
+import adminDataReducer from './adminDataSelectionSlice';
+import adminEventsReducer from './adminEventsSelectionSlice';
+import adminReportsReducer from './adminReportsSelectionSlice';
+import adminReducer from './adminSelectionSlice';
+import dashboardReducer from './dashboardSelectionSlice';
+import globalReducer from './globalSelectionSlice';
+import reportReducer from './reportSelectionSlice';
+import userActivityReducer from './userActivitySelectionSlice';
 
-const item = (id: number) => ({ id, name: `item-${id}` }) as never;
-const seller = (sellerId: number) =>
-  ({
-    sellerId,
-    sellerName: `Seller ${sellerId}`,
-    sellerType: 0,
-  }) as never;
-
-// remaining file unchanged
+describe('selection slice reducers', () => {
+  it('returns initial state for each reducer', () => {
+    expect(adminDataReducer(undefined, { type: 'test/init' })).toBeDefined();
+    expect(adminEventsReducer(undefined, { type: 'test/init' })).toBeDefined();
+    expect(adminReportsReducer(undefined, { type: 'test/init' })).toBeDefined();
+    expect(adminReducer(undefined, { type: 'test/init' })).toBeDefined();
+    expect(dashboardReducer(undefined, { type: 'test/init' })).toBeDefined();
+    expect(globalReducer(undefined, { type: 'test/init' })).toEqual({
+      isLoading: false,
+      saveInProgress: false,
+    });
+    expect(reportReducer(undefined, { type: 'test/init' })).toBeDefined();
+    expect(userActivityReducer(undefined, { type: 'test/init' })).toBeDefined();
+  });
+});
